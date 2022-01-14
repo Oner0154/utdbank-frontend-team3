49887cff6f4db9e2b876a0884461a053822a28b1
Salih Bayram <alicamci2020@gmail.com>
2022-01-14 15:37:27 +0300

    V1

---
+++ src/components/home/services/ServiceItem.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const ServiceItem = ({ title, description, image, link }) => {
+  return (
+    <div className="home-service-item fluid-height">
+      <div className="home-service-details full-height">
+        <div className="home-service-image">
+          <img src={`assets/images/${image}`} alt="service" />
+        </div>
+        <div className="home-service-text">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <a href={link} target="_blank">
+            Read More +
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ServiceItem
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
 import "alertifyjs/build/css/alertify.min.css"
 import "react-toastify/dist/ReactToastify.min.css"
@@ -11,10 +11,10 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
