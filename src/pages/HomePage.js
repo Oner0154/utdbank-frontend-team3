9607bc8caaef67666ae6a92f5c259dd0dfc3bb8e
Oner <tahirugur2628@gmail.com>
2022-01-15 07:12:35 +0100

    day2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
-import React from "react"
-import Topbar from "../components/common/Topbar"
-import Banner from "../components/home/banner/Banner"
-import BannerContact from "../components/home/banner/BannerContact"
-import Counter from "../components/home/Counter"
-import Features from "../components/home/Features"
-import HomeAbout from "../components/home/HomeAbout"
+import React from "react";
+import Topbar from "../components/common/Topbar";
+import Banner from "../components/home/banner/Banner";
+import BannerContact from "../components/home/banner/BannerContact";
+import Counter from "../components/home/Counter";
+import Features from "../components/home/Features";
+import HomeAbout from "../components/home/HomeAbout";
+import Pricing from "../components/home/Pricing";
+import Services from "../components/home/services/Services";
 
 const HomePage = () => {
   return (
@@ -15,8 +17,10 @@ const HomePage = () => {
       <Features />
       <HomeAbout />
       <Counter />
+      <Services />
+      <Pricing />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
